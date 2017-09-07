d982f9ee9b44da463e81adeefccbb6b4ae0238b0
Thomas <t.massonn@gmail.com>
2017-09-07 12:12:18 +0200

    suppression mon play
    suppression

--- assets/parts/footer.php
+++ assets/parts/footer.php
@@ -1,30 +1,4 @@
 	<footer class="footer">
-	
-<<<<<<< HEAD
-	<div class="audio-player">
-				
-    	<div id="play-btn"></div>
-    	<div class="audio-wrapper" id="player-container" href="javascript:;">
-      	<audio id="player" ontimeupdate="initProgressBar()">
-			  <source src="http://www.lukeduncan.me/oslo.mp3" type="audio/mp3">
-			</audio>
-		</div>
-		<span id="skip-btn"></span>
-    	<div class="player-controls scrubber">
-		
-		<p><img src="https://unsplash.it/50/50/?random"></img>
-		Oslo <small>by</small> Holy Esque</p>
-      	<span id="seekObjContainer">
-			  <progress id="seekObj" value="0" max="1"></progress>
-			</span>
-      	<br>
-      	<small style="float: left; position: relative; left: 15px;" class="start-time"></small>
-      	<small style="float: right; position: relative; right: 20px;" class="end-time"></small>
-		
-    </div>
-=======
-
->>>>>>> de139c2992f3ede63b0578460ead986b0ba148b0
     
     </footer>
 	
