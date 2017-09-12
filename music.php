a99f84708480b73aa640b40d1defb8b9d243a0a5
Thomas <t.massonn@gmail.com>
2017-09-12 11:36:09 +0200

    Ajout test parti music commentaire

---
+++ music.php
@@ -0,0 +1,31 @@
+<div class="col-md-12">
+    <div class="col-md-9">
+        <h2>Titre</h2>
+        <h4>by Artiste</h4>
+        <p>Description :  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu sapien luctus, consectetur magna et, interdum nisl. Integer vestibulum enim ac justo faucibus, eget mattis diam molestie. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla ut vehicula nulla, ac placerat velit. Sed pellentesque ex volutpat ultricies pharetra. Praesent vel sollicitudin odio, vitae mollis diam. Praesent quis elit ac dui fringilla maximus. Nullam suscipit arcu id ultricies varius.</p>
+    </div>
+    <div class="col-md-3">
+        <img src="https://unsplash.it/200/200/?random">
+    </div>
+</div>
+<div class="col-md-6">
+<div class="jumbotron">
+<div class="container">
+    <div class="input-group">
+        <input type="text" id="userComment" class="form-control input-sm chat-input" placeholder="Write your message here..." />
+	    <span class="input-group-btn" onclick="addComment()">     
+            <a href="#" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-comment"></span> Add Comment</a>
+        </span>
+    </div>
+    <hr data-brackets-id="12673">
+    <ul data-brackets-id="12674" id="sortable" class="list-unstyled ui-sortable">
+        <strong class="pull-left primary-font"><!--User -->User</strong>
+        <small class="pull-right text-muted">
+           <span class="glyphicon glyphicon-time"></span><!-- time --></small>
+        </br>
+        <li class="ui-state-default">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </li>
+        
+    </ul>
+    </div>
+    </div>
+    </div>
