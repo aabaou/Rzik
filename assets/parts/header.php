cc296969d2989007be336778a8843b3d748d5abc
Thomas <t.massonn@gmail.com>
2017-10-09 11:29:03 +0200

    push r

--- assets/parts/header.php
+++ assets/parts/header.php
@@ -17,7 +17,6 @@
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/tools.css">
-		<link rel="stylesheet" type="text/css" href="assets/css/ui.css">
 		<!-- END CSS -->
 
 		<!-- Favicon -->
@@ -29,8 +28,6 @@
         
         <!-- JQuery -->
 		<script src="assets/js/jquery-3.2.1.min.js" type="text/javascript" charset="utf-8"></script>
-		<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" type="text/javascript" charset="utf-8"></script>
-		<script src="assets/js/list.js" type="text/javascript" charset="utf-8"></script>
 	</head>
   <script>
     window.fbAsyncInit = function() {
@@ -123,8 +120,6 @@
   </script>
 	<body>
 
-	<input id="lang" type="hidden" value="<?php echo $_SESSION['lang'] ?>">
-
 		<!-- Fixed navbar -->
 		<nav id="header" class="navbar navbar-fixed-top">
 		    <div id="header-container" class="container navbar-container">
@@ -158,15 +153,12 @@
 									?> 
 									<span class="caret"></span>
 								</a>
-
-								<ul id="login-dp" class="dropdown-menu">
-									<div class="row">
-										<div class="col-md-12">
-											<form class="form" role="form" method="post" action="assets/ws/deconnexion.php" accept-charset="UTF-8" id="login-nav">
-												<div class="form-group">
-													<button type="submit" class="btn btn-primary btn-block"><?php echo($dico['disconnect']); ?></button>
-												</div>
-											</form>
+			<ul id="login-dp" class="dropdown-menu">
+					 <div class="row">
+							<div class="col-md-12">
+								 <form class="form" role="form" method="post" action="assets/ws/deconnexion.php" accept-charset="UTF-8" id="login-nav">
+										<div class="form-group">
+											 <button type="submit" class="hvr-horizontal blue">Disconnect</button>
 										</div>
 									</div>
 									<?php
